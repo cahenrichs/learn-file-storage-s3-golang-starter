1223753851e871305b95d03f757609fda16786cd
cahenrichs <cahenrichs@gmail.com>
2026-01-29 22:55:46 +0000

    edited some lines for the s3 bucket

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,8 +1,18 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
 	"net/http"
-	
+	"mime"
+	"os"
+	"path/filepath"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
 )
@@ -44,5 +54,58 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		respondWithError(w, http.StatusUnauthorized, "Not authorized to update this video", err)
 		return
 	}
+
+	//Parsing the uploaded video file from the form
+	file, header, err := r.FormFile("video")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	//Validate the uploaded file to ensure its MP4
+	mediaType, _, err := mime.ParseMediaType(handler.Header.Get("Content-Type"))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid content type", err)
+		return
+	} 
+	if mediaType != "video/mp4" {
+		respondWithError(w, http.StatusBadRequest, "Invalid file type, only mp4 is allowed", nil)
+		return
+	}
+
+	// Saving the uploaded file to a temp file on disk
+	tempFile, err := os.CreateTemp("", "tubley-upload.mp4")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not create temp file", err)
+		return
+	}
+	defer os.Remove(tempFile.Name)
+	defer tempFile.Close()
+
+	if _, err := io.Copy(tempFile, file); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't write file to disk", err)
+		return
+	}
+
+	//Reset tempFiles pointer to the begining
+	_, err = temp.Seek(0, io.SeekStart)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not reset file pointer", err)
+		return
+	}
+
+	//Putting the object into s3
+	_, err = client.PutObject(ctx, &s3.PutObjectInput{
+		Bucket:		aws.String(cfg.s3Bucket),
+		Key:		aws.String(key),
+		Body:		tempFile,
+		ContentType: aws.String(mediaType),
+	})
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error uploading file to s3", err)
+		return
+	}
+	
 
 }
